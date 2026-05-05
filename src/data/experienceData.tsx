eb03c8d4c9e97dcce30e0352f738b713f7ee7c99
jumi <a25320@naver.com>
2026-05-05 14:58:42 +0900

    experienceData 추가 및 index.ts에서 내보내기 수정

---
+++ src/data/experienceData.tsx
@@ -0,0 +1,22 @@
+export const experienceData = [
+  {
+    company: '에스피지이',
+    position: '프론트엔드 개발자',
+    period: '2022.09 ~ ',
+    description: '프렌차이즈 관리 시스템 개발 및 유지보수'
+  },
+  {
+    company: '플레버스',
+    position: '프론트엔드 개발자',
+    period: '2021.03 ~ 2022.02',
+    description: '게임 아이템 거래를 위한 플랫폼 개발',
+    reason: '경영악화'
+  },
+  {
+    company: '이브필라테스',
+    position: '프론트엔드 개발자',
+    period: '2020.08 ~ 2021.03',
+    description: '자사 쇼핑몰 웹 개발 및 유지보수',
+    reason: '경영악화'
+  }
+]
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,2 +1,3 @@
 export * from './projectData'
 export * from './skillsData'
+export * from './experienceData'
